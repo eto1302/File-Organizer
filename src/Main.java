b139ad305783623bfc80df1975f521abd274c61b
Martin Angelov <m.angelov-1@student.tudelft.nl>
2023-05-05 10:11:12 +0200

    App now runs in System Tray

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,18 +9,49 @@ public class Main {
 
     public static void main(String[] args) {
         Initialize();
+        PutToSystemTray();
+    }
+
+    private static void PutToSystemTray() {
+        if (!SystemTray.isSupported()) {
+            Logger.Log("System tray is not supported!");
+            return;
+        }
+
+        SystemTray systemTray = SystemTray.getSystemTray();
+
+        Image image = Toolkit.getDefaultToolkit().getImage("Resources/icon.png");
+
+        PopupMenu trayPopupMenu = new PopupMenu();
+
+        MenuItem close = new MenuItem("Close");
+        close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        trayPopupMenu.add(close);
+        TrayIcon trayIcon = new TrayIcon(image, "SystemTray Demo", trayPopupMenu);
+        trayIcon.setImageAutoSize(true);
+
+        try {
+            systemTray.add(trayIcon);
+        } catch (AWTException awtException) {
+            Logger.LogException(awtException);
+        }
     }
 
     private static void Initialize() {
         DirectoryThread[] directoryThreads = IO.getDirectoryThreads();
-        for(DirectoryThread thread : directoryThreads){
+        for (DirectoryThread thread : directoryThreads) {
             OrganizeFiles(thread.getPath());
             thread.start();
         }
     }
 
     private static void OrganizeFiles(Path path) {
-        try{
+        try {
             Files.walk(path, 1).forEach(file -> {
                 try {
                     IO.moveFile(path, file);
@@ -25,8 +59,7 @@ public class Main {
                     Logger.LogException(e);
                 }
             });
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             Logger.LogException(e);
         }
     }
